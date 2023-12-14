8f0b730d7e9fb6c1196a3aad746312d031e929bf
Kenin Kujovic <kujovic.kenin@gmail.com>
2023-12-14 23:40:18 +0100

    FEATURE: Add unique name generation

--- main.tf
+++ main.tf
@@ -2,18 +2,40 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.64.0"
+      version = ">= 4.64.0"
+    }
+    //Random provider used for randstring generation
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0.0"
     }
   }
 }
 
 provider "aws" {}
 
-resource "aws_s3_bucket" "imageresizerbeforeninethousand" {
-  bucket = "imageresizerbeforeninethousand"
+data "aws_caller_identity" "current" {}
+
+//Random id for name clash risk reduction
+resource "random_id" "generated" {
+  byte_length = 8
 }
 
-resource "aws_s3_bucket" "imageresizerafterninethousand" {
-  bucket = "imageresizerafterninethousand"
+//Image original size bucket
+resource "aws_s3_bucket" "imageResizerBefore" {
+  bucket = "imageresizerbefore-${random_id.generated.hex}"
 }
 
+//Image resized size bucket
+resource "aws_s3_bucket" "imageResizerAfter" {
+  bucket = "imageresizerafter-${random_id.generated.hex}"
+}
+
+//Aws Lambda function
+resource "aws_lambda_function" "image_resizer_lambda_ninethousand" {
+  function_name = "imageresizerlambda-${random_id.generated.hex}"
+  filename      = "index.zip"
+  handler       = "index.handler"
+  runtime       = "nodejs16.x"
+  role          = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/LabRole"
+}
